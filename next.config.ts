e8af00e991c45623e7c31834cc3bdd8a597705ed
k0hei2025 <vaibhavvermaonline@gmail.com>
2025-03-06 22:07:41 +0530

    feat: fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  basePath: process.env.NODE_ENV === 'production' ? '/simpson-2025-production' : '',
+  output: 'export',
 };
 
 export default nextConfig;
